c12b641d142e95e3dfb931b01b07aa518fe448dc
Binayak Bishnu <bishnu.binayak12@gmail.com>
2023-10-25 20:48:22 +0530

    fix: added conditionals for tileData

--- src/GridLayout.js
+++ src/GridLayout.js
@@ -15,23 +15,23 @@ function GridLayout() {
             <Grid bg={'gray.200'}
                 templateColumns={{ sm: 'repeat(1,fr)', sm: 'repeat(4, 1fr)', md: 'repeat(6, 1fr)' }} gap={4} padding={2}>
                 {
-                    tiles.skills.map((tileData, index) => {
+                    tiles?.skills?.map((tileData, index) => {
                         return (
-                            <GridItem key={index} id={`${index}-${tileData.tileName}`} /* w='100%' */ minHeight='120px'
+                            <GridItem key={index} id={`${index}-${tileData?.tileName}`} /* w='100%' */ minHeight='120px'
                                 display={'flex'} flexDirection={'column'} alignItems="flex-start" justifyContent="space-between"
                                 boxShadow={'md'} bg='white' colSpan={2} p={2} borderRadius='lg' position='relative'>
 
                                 <Flex width={'100%'} alignItems={'flex-start'} justifyContent={'space-between'}>
                                     <Heading as='h3' size='md' maxWidth={'60%'}>
-                                        {tileData.displayTileName}
+                                        {tileData?.displayTileName}
                                     </Heading>
 
                                     <Box flex={1} bg={'blue.500'} position={'relative'}/*  alignItems="center" justifyContent="flex-start" wrap='wrap' rowGap={0} columnGap={0} */>
                                         {
-                                            tileData.providerInfo.length <= 4 &&
-                                            tileData.providerInfo.map((provider, index) => {
+                                            tileData?.providerInfo?.length <= 4 &&
+                                            tileData?.providerInfo?.map((provider, index) => {
                                                 return (
-                                                    <Box key={index} position={'absolute'} right={(tileData.providerInfo.length * 4) - ((index + 1) * 4)} top={0}>
+                                                    <Box key={index} position={'absolute'} right={(tileData?.providerInfo?.length * 4) - ((index + 1) * 4)} top={0}>
                                                         <Image src={`${provider.profileImage}`} alt={`${provider.providerId}`}
                                                             height={'30px'} width={'30px'} borderRadius={'full'} border='2px' borderColor='white' />
                                                     </Box>
@@ -41,10 +41,10 @@ function GridLayout() {
 
                                         {/* for more than 4 providers */}
                                         {
-                                            tileData.providerInfo.length > 4 &&
-                                            tileData.providerInfo.map((provider, index) => {
+                                            tileData?.providerInfo?.length > 4 &&
+                                            tileData?.providerInfo?.map((provider, index) => {
                                                 return (
-                                                    <Box key={index} position={'absolute'} right={(tileData.providerInfo.length * 4) - ((index) * 4)} top={0}>
+                                                    <Box key={index} position={'absolute'} right={(tileData?.providerInfo?.length * 4) - ((index) * 4)} top={0}>
                                                         <Image src={`${provider.profileImage}`} alt={`${provider.providerId}`}
                                                             height={'30px'} width={'30px'} borderRadius={'full'} border='2px' borderColor='white' />
                                                     </Box>
@@ -52,35 +52,33 @@ function GridLayout() {
                                             })
                                         }
                                         {
-                                            tileData.providerInfo.length > 4 &&
+                                            tileData?.providerInfo?.length > 4 &&
                                             <Icon as={BsPlusCircleFill} color={'gray.500'} width={'30px'} height={'30px'} position={'absolute'} right={0} />
                                         }
                                     </Box>
                                 </Flex>
 
-                                {tileData.moreProvidersAvailable ? 'true' : ''}
-
                                 {
-                                    tileData.availability.startTime && tileData.availability.endTime &&
+                                    tileData?.availability?.startTime && tileData?.availability?.endTime &&
                                     <Flex width={'100%'} alignItems={'center'} justifyContent={'flex-end'} columnGap={1}>
                                         <Text fontSize={'xs'} fontWeight={'bold'} align={'right'} color={'green.600'}>
                                             {
                                                 new Date() ===
-                                                    new Date(tileData.availability.startTime) ?
+                                                    new Date(tileData?.availability?.startTime) ?
                                                     'Now' :
                                                     new Date().getDate() + 1 ===
-                                                        new Date(tileData.availability.startTime).getDate() ?
+                                                        new Date(tileData?.availability?.startTime).getDate() ?
                                                         'Tomorrow' :
-                                                        new Date(tileData.availability?.startTime).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })
+                                                        new Date(tileData?.availability?.startTime).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })
                                             }
-                                            -{new Date(tileData.availability?.endTime).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })}
+                                            -{new Date(tileData?.availability?.endTime).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })}
                                         </Text>
                                         <Icon as={BsAlarm} color={'green.600'} />
                                     </Flex>
                                 }
 
                                 <Box position='absolute' left={'-0.1rem'} top={'-0.1rem'}
-                                    bg={`${tileData.availability.color}`} padding={0} margin={0}
+                                    bg={`${tileData?.availability?.color}`} padding={0} margin={0}
                                     borderRadius={'full'} height={'10px'} width={'10px'}>
                                 </Box>
                             </GridItem>
